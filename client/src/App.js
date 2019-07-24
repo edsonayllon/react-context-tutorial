1ddf9466387415a36f7d4879b4ff69892454e0a1
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-07-24 06:08:34 +0000

    added a Context object

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ThemeContext, themes } from './contexts/ThemeContext';
 
 function App() {
   return (
+    <ThemeContext.Provider value={themes.dark}>
+      <Child />
+    </ThemeContext.Provider>
+  );
+}
+
+function Child() {
+  const theme = useContext(ThemeContext);
+  console.log(theme);
+  console.log(ThemeContext);
+  return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{backgroundColor: theme.background}}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,7 +32,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
